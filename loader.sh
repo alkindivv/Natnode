0e7e64e107f1ed5758e82abd03ea783eed7b5de1
0xalkindivv.eth <66145311+alkindivv@users.noreply.github.com>
2024-10-08 08:33:47 +0700

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -3,17 +3,30 @@ tput civis
 
 # Clear Line
 CL="\e[2K"
+# Spinner Character
+SPINNER="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
 
-echo "╔══════════════════════════════════════════════════════════════════╗"
-echo "║  ███╗   ██╗ █████╗ ████████╗███╗   ██╗ ██████╗ ██████╗ ███████╗  ║"
-echo "║  ████╗  ██║██╔══██╗╚══██╔══╝████╗  ██║██╔═══██╗██╔══██╗██╔════╝  ║"
-echo "║  ██╔██╗ ██║███████║   ██║   ██╔██╗ ██║██║   ██║██║  ██║█████╗    ║"
-echo "║  ██║╚██╗██║██╔══██║   ██║   ██║╚██╗██║██║   ██║██║  ██║██╔══╝    ║"
-echo "║  ██║ ╚████║██║  ██║   ██║   ██║ ╚████║╚██████╔╝██████╔╝███████╗  ║"
-echo "║  ╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝  ║"
-echo "║                                                                  ║"
-echo "║           ◢◤◢◤◢◤ https://github.com/alkindivv ◥◣◥◣◥◣             ║"
-echo "║                                                                  ║"
-echo "║  • Blockchain        • Web3, Defi            • Node Runner       ║"
-echo "║  • API Integration   • Testnet/Retroactive   • Validator Runner  ║"
-echo "╚══════════════════════════════════════════════════════════════════╝"
+function spinner() {
+  task=$1
+  msg=$2
+  while :; do
+    jobs %1 > /dev/null 2>&1
+    [ $? = 0 ] || {
+      printf "${CL}✓ ${task} Done\n"
+      break
+    }
+    for (( i=0; i<${#SPINNER}; i++ )); do
+      sleep 0.05
+      printf "${CL}${SPINNER:$i:1} ${task} ${msg}\r"
+    done
+  done
+}
+
+msg="${2-InProgress}"
+task="${3-$1}"
+$1 & spinner "$task" "$msg"
+
+tput cnorm
+
+# usage => ./loader.sh "<TIMER_TO_SLEEP>" "<PROGRESS>" "<TASK_NAME>"
+# e.g => ./loader.sh "sleep 5" "..." "Installing Dependencies"
